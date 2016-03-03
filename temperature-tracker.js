d7e4e9aa8fff21a25780d16b892ff81438b1ac86
Andrea Stella <adr.stella@gmail.com>
2016-03-03 12:19:55 -0800

    Add min/max/mean/mode problem

--- temperature-tracker.js
+++ temperature-tracker.js
@@ -5,7 +5,47 @@
 function TempTracker() {
   return {
     temps: [],
-    insert: (temp) => this.temps.push(temp),
-    getMax: () => Math.max(...this.temps),
+    insert: function(temp) {return this.temps.push(temp)},
+    getMax: function() {return Math.max(...this.temps)},
+    getMin: function() {return Math.min(...this.temps)},
+    getMean: function() {return this.temps.reduce((a, b) => a + b) / this.temps.length},
+    getMode: function() {
+      const nums = {};
+      let mostAppearances = 1;
+
+      this.temps.forEach((temp) => {
+        nums[temp] = nums[temp] || 0;
+        nums[temp]++;
+      });
+
+      Object.keys(nums).forEach((key) => {
+        if (nums[key] > mostAppearances) mostAppearances = nums[key];
+      });
+
+      if (mostAppearances < 2) return [];
+      else return Object.keys(nums).reduce((acc, key) => {
+        if (nums[key] === mostAppearances) acc.push(key);
+        return acc;
+      }, []);
+    }
   }
 }
+
+const tracker = TempTracker();
+
+tracker.insert(3);
+tracker.insert(4);
+tracker.insert(4);
+tracker.insert(4);
+tracker.insert(72);
+tracker.insert(45);
+tracker.insert(23);
+tracker.insert(23);
+tracker.insert(23);
+tracker.insert(21);
+tracker.insert(20);
+
+console.log('max', tracker.getMax());
+console.log('min', tracker.getMin());
+console.log('mean', tracker.getMean());
+console.log('mode', tracker.getMode()); // Wrong! should only return one.
